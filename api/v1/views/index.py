cf456c44b7a80fe8bc58955e7b42207802a08ebe
Frank <wanyoike39@gmail.com>
2023-07-28 13:02:01 +0300

    fixing requirement fail for index.py - fix 2

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,11 +5,13 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
+    """endpoint returns status OK"""
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
+    """endpoint retrieves the number of each objects by type"""
     from models import storage
     from models.state import State
     from models.city import City
